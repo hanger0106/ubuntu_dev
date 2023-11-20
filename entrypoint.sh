b76127fa73d91fcb90b970194bbc4d85e3cfc7d0
simon_shih <simon_shih@accton.com>
2023-11-20 11:55:41 +0800

    del not working, try usermod

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ ID=${USER_ID:-1000}
 if [ $ID -eq `id -u` ]; then
     exec "$@"
 else
-    userdel $USER
     useradd --shell /bin/bash --home-dir $WORKDIR -u $ID $USER
+    usermod -u $ID $USER
     exec gosu $ID "$@"
 fi
