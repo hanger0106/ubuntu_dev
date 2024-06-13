e3472a2f6ffc82cf7c4f54474f1869605e75d97f
simon_shih <simon_shih@accton.com>
2024-06-13 15:22:43 +0800

    try userdel default user

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,10 @@ ID=${USER_ID:-1000}
 if [ $ID -eq `id -u` ]; then
     exec "$@"
 else
+    #Not defaul user
+    userdel -r user
     useradd --shell /bin/bash --home-dir $WORKDIR -u $ID $USER
     usermod -u $ID $USER
+    adduser $USER sudo
     exec gosu $ID "$@"
 fi
