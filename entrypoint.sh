3a313f1577c2ee90b85cff3abf285bad38978e8e
simon_shih <simon_shih@accton.com>
2024-07-08 12:20:26 +0800

    sync defualt user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
+DEFAULT_USER=user
 USER=${USER_NAME:-user}
 ID=${USER_ID:-1000}
 
 if [ $ID -eq `id -u` ]; then
     exec "$@"
+elif [ "$USER" == "$DEFAULT_USER"  ]; then
+    #Not defaul uid
+    usermod -u ${ID} ${USER}
+    exec gosu $ID "$@"
 else
     #Not defaul user
     userdel user
@@ -12,4 +16,3 @@ else
     adduser $USER sudo
     exec gosu $ID "$@"
 fi
-
